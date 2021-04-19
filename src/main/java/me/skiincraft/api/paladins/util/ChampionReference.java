6c19d5f5222fae7ad6cbbb252e0d5d3fc0770b00
Cristian "SknZ" Ferreira <61399406+Cristian-Sknz@users.noreply.github.com>
2021-04-19 19:20:35 -0300

    [util] Uma classe onde tem todos os Ids dos champions.

---
+++ src/main/java/me/skiincraft/api/paladins/util/ChampionReference.java
@@ -0,0 +1,61 @@
+package me.skiincraft.api.paladins.util;
+
+/** <h3>Champion Reference</h3>
+ * Contains all the champions of the game
+ * It will be noted with deprecated as it will not always be updated.
+ * This can be very useful for some people.
+ *
+ * @deprecated It will not always be up to date
+ */
+@Deprecated
+public class ChampionReference {
+
+    public static final long Androxus = 2205;
+    public static final long Ash = 2404;
+    public static final long Atlas = 2512;
+    public static final long Barik = 2073;
+    public static final long Bomb_King = 2281;
+    public static final long Buck = 2147;
+    public static final long Cassie = 2092;
+    public static final long Corvus = 2533;
+    public static final long Dredge = 2495;
+    public static final long Drogoz = 2277;
+    public static final long Evie = 2094;
+    public static final long Fernando = 2071;
+    public static final long Furia = 2491;
+    public static final long Grohk = 2093;
+    public static final long Grover = 2254;
+    public static final long Imani = 2509;
+    public static final long Inara = 2348;
+    public static final long Io = 2517;
+    public static final long Jenos = 2431;
+    public static final long Khan = 2479;
+    public static final long Kinessa = 2249;
+    public static final long Koga = 2493;
+    public static final long Lex = 2362;
+    public static final long Lian = 2417;
+    public static final long Maeve = 2338;
+    public static final long Makoa = 2288;
+    public static final long Mal_Damba = 2303;
+    public static final long Moji = 2481;
+    public static final long Octavia = 2540;
+    public static final long Pip = 2056;
+    public static final long Raum = 2528;
+    public static final long Ruckus = 2149;
+    public static final long Seris = 2372;
+    public static final long Sha_Lin = 2307;
+    public static final long Skye = 2057;
+    public static final long Strix = 2438;
+    public static final long Talus = 2472;
+    public static final long Terminus = 2477;
+    public static final long Tiberius = 2529;
+    public static final long Torvald = 2322;
+    public static final long Tyra = 2314;
+    public static final long Viktor = 2285;
+    public static final long Vivian = 2480;
+    public static final long Vora = 2536;
+    public static final long Willo = 2393;
+    public static final long Yagorath = 2538;
+    public static final long Ying = 2267;
+    public static final long Zhin = 2420;
+}
